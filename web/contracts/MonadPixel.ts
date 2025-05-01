e63574a2758178e0417c807e8ecd6bca2e71fc14
Bean <bean1730137@gmail.com>
2025-05-01 11:55:27 +0800

    feat: abi

--- web/contracts/MonadPixel.ts
+++ web/contracts/MonadPixel.ts
@@ -2,118 +2,40 @@ import { Contract } from "ethers";
 
 export const MonadPixelABI = [
   {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "x",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "y",
-        type: "uint256",
-      },
-      {
-        internalType: "string",
-        name: "color",
-        type: "string",
-      },
-    ],
-    name: "mint",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "x",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "y",
-        type: "uint256",
-      },
-      {
-        internalType: "string",
-        name: "color",
-        type: "string",
-      },
-    ],
-    name: "paint",
-    outputs: [],
+    inputs: [],
     stateMutability: "nonpayable",
-    type: "function",
+    type: "constructor",
   },
   {
+    anonymous: false,
     inputs: [
       {
+        indexed: false,
         internalType: "uint256",
         name: "x",
         type: "uint256",
       },
       {
+        indexed: false,
         internalType: "uint256",
         name: "y",
         type: "uint256",
       },
-    ],
-    name: "getPixel",
-    outputs: [
       {
+        indexed: false,
         internalType: "string",
         name: "color",
         type: "string",
       },
       {
+        indexed: false,
         internalType: "address",
         name: "owner",
         type: "address",
       },
     ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getAllPixels",
-    outputs: [
-      {
-        components: [
-          {
-            internalType: "string",
-            name: "color",
-            type: "string",
-          },
-          {
-            internalType: "address",
-            name: "owner",
-            type: "address",
-          },
-        ],
-        internalType: "struct MonadPixel.Pixel[10][10]",
-        name: "",
-        type: "tuple[10][10]",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "printAllPixels",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "withdraw",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
+    name: "PixelInfo",
+    type: "event",
   },
   {
     anonymous: false,
@@ -172,35 +94,118 @@ export const MonadPixelABI = [
     type: "event",
   },
   {
-    anonymous: false,
+    inputs: [],
+    name: "getAllPixels",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "string",
+            name: "color",
+            type: "string",
+          },
+          {
+            internalType: "address",
+            name: "owner",
+            type: "address",
+          },
+        ],
+        internalType: "struct MonadPixel.Pixel[10][10]",
+        name: "",
+        type: "tuple[10][10]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
     inputs: [
       {
-        indexed: false,
         internalType: "uint256",
         name: "x",
         type: "uint256",
       },
       {
-        indexed: false,
         internalType: "uint256",
         name: "y",
         type: "uint256",
       },
+    ],
+    name: "getPixel",
+    outputs: [
       {
-        indexed: false,
         internalType: "string",
         name: "color",
         type: "string",
       },
       {
-        indexed: false,
         internalType: "address",
         name: "owner",
         type: "address",
       },
     ],
-    name: "PixelInfo",
-    type: "event",
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "x",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "y",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "color",
+        type: "string",
+      },
+    ],
+    name: "mint",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "x",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "y",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "color",
+        type: "string",
+      },
+    ],
+    name: "paint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "printAllPixels",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
   },
 ];
 
